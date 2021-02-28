7d894978e3e6028f78e3155e4f7f7a2397973329
Joliage <79417369+Joliage@users.noreply.github.com>
2021-02-28 01:41:21 -0600

    Add files via upload
    The good one

--- shared1.h
+++ shared1.h
@@ -2,5 +2,17 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <unistd.h>
-#include<time.h> 
+#include <time.h> 
 
+//#include <bits/stdc++.h> 
+//using namespace std; 
+  
+#define COMPUTER 1 
+#define HUMAN 2 
+  
+#define SIDE 3 // Length of the board 
+  
+// Computer will move with 'O' 
+// and human with 'X' 
+#define COMPUTERMOVE 'O' 
+#define HUMANMOVE 'X' 
